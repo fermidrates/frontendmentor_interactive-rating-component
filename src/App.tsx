9e68c74f2f6173b9982b9293f25d7c13cec08a00
fermidrates <albertfarand@gmail.com>
2022-03-30 16:29:44 +0700

    feat: image, fix: fonts, hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+  const [ratingSelected, setRatingSelected] = useState<Number>(0);
+  const ratings = [1, 2, 3, 4, 5];
+
+  if (isSubmitted === false) {
+    return (
+      <>
+      <div className="rating__modal">
+        <div className="rating__image">
+          <img src="icon-star.svg" alt="star" />
+        </div>
+        <h2>How did we do?</h2>
+      
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Please let us know how we did with your support request. All feedback is appreciated 
+          to help us improve our offering!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      
+        <div className="rating__value">
+          {ratings.map((rating: number) => (
+            ratingSelected === rating
+            ? <div key={rating} className="rating__values" id="selected" onClick={() => setRatingSelected(rating)}>{rating}</div>
+            : <div key={rating} className="rating__values" onClick={() => setRatingSelected(rating)}>{rating}</div>
+          ))}
+        </div>
+        <button className="rating__button" onClick={() => setIsSubmitted(true)}>
+          SUBMIT
+        </button>
+      </div>
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/fermidrates">Farand Albert S</a>.
+      </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+      {/* <!-- Thank you state start --> */}
+      <div className="rating__confirmation">
+        <img src="illustration-thank-you.svg" alt="terimakasih" />
+        <div className="rating__chosen">
+          You selected {ratingSelected} out of 5
+        </div>
+      
+        <h2>Thank you!</h2>
+
+        <p>
+          We appreciate you taking the time to give a rating. If you ever need more support, 
+          don’t hesitate to get in touch!
+        </p>
+      </div>
+      
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/fermidrates">Farand Albert S.</a>.
+      </div>
+      </>
+    )
+  }
 }
 
 export default App;
